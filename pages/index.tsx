51747cb4afdc8de5c28c7a107b1914a906adc01a
AndreySchmidt <ringnull@gmail.com>
2023-07-15 22:58:51 +0300

    Auth page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import AuthPage from '@/components/templates/AuthPage/AuthPage'
 import Head from 'next/head'
 
 export default function Auth() {
@@ -10,6 +11,7 @@ export default function Auth() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" type="image/svg" sizes="32x32" href="/img/logo.svg" />
       </Head>
+      <AuthPage />
     </>
   )
 }
--- readme.txt
+++ readme.txt
@@ -1,2 +1,5 @@
 npm create next-app --typescript
 npm i -D @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint-config-prettier eslint-plugin-prettier  prettier
+npm i axios
+npm i sass
+npm add react-hook-form
